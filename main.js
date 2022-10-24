d7aa254e0b07050152e0845298e58e27cc23409d
lcx <601271628@qq.com>
2022-10-24 10:29:13 +0800

    轮播图2

--- main.js
+++ main.js
@@ -5,6 +5,33 @@ Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+// axios
+// import "./util/request.js"
+
+// 老师的http
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+$http.baseUrl = 'https://www.uinav.com'
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  wx.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  wx.hideLoading()
+}
+
+// toast
+uni.$showMessage = function(title="网络错误",duration=800){
+    uni.showToast({
+        title,
+        duration,
+        icon:'none'
+    })
+}
+                        
 const app = new Vue({
 	...App
 })
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		home
+        <!-- 轮播图 -->
+        <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="500">
+                <swiper-item v-for="item in swipperList" :key="item.goods_id">
+                    <navigator :url="'../../subpkg/good_detail/good_detail?good_id=' + item.goods_id">
+                        <image :src="item.image_src" mode="widthFix"></image>
+                    </navigator>
+                </swiper-item>
+        </swiper>
 	</view>
 </template>
 
@@ -8,12 +15,30 @@
 	export default {
 		data() {
 			return {
-				
+				swipperList:[]
 			};
-		}
+		},
+        onLoad(){
+              this.getSwiperList()
+              console.log(this.swipperList);
+        },
+        methods:{
+            // 轮播图
+            getSwiperList(){
+                uni.$http.get('/api/public/v1/home/swiperdata').then(res=>{
+                    console.log(res);
+                    if(res.statusCode!==200){
+                        return uni.$showMessage() // 自定义全局toast
+                    }
+                    this.swipperList = res.data.message
+                })
+            }
+        }
 	}
 </script>
 
 <style lang="scss">
-
+ swiper-item image{
+     width: 100%;
+ }
 </style>
