93c059551bd6d7bd1a5b4a3902afe91068df52f4
lcx <601271628@qq.com>
2022-10-24 09:57:35 +0800

    轮播图

---
+++ util/request.js
@@ -0,0 +1,22 @@
+// import Vue from "vue"
+// import axios from "axios"
+// import nprogress from "nprogress"
+
+// // Vue.prototype.$axios = axios
+// const service = axios.create({
+//     // baseURL: 'http://localhost:3006',
+//     // baseURL:'https://www.uinav.com',
+//     timeout: 5000,
+//     withCredentials: true,
+// })
+
+// service.interceptors.request.use(config=>{
+//     nprogress.start()
+//     return confiig
+// })
+// service.interceptors.response.use(res=>{
+//   nprogress.done()  
+//   return res
+// })
+
+// export default service
