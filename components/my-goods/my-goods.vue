a0501ac204bd81dd263aa89936fb4c1e16254d6a
lcx <601271628@qq.com>
2022-10-25 20:49:57 +0800

    goodList第二次提交

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -40,34 +40,34 @@
 </script>
 
 <style lang="scss">
-.goods-item {
-  display: flex;
-  padding: 10px 5px;
-  border-bottom: 1px solid #f0f0f0;
-
-  .goods-item-left {
-    margin-right: 5px;
-
-    .goods-pic {
-      width: 100px;
-      height: 100px;
-      display: block;
-    }
-  }
-
-  .goods-item-right {
+  .goods-item {
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
 
-    .goods-name {
-      font-size: 13px;
+    .goods-item-left {
+      margin-right: 5px;
+
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
     }
 
-    .goods-price {
-      font-size: 16px;
-      color: #c00000;
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+      }
+
+      .goods-price {
+        font-size: 16px;
+        color: #c00000;
+      }
     }
   }
-}
 </style>
